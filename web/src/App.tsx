10f795bc8d4a01c6d296104773c98043e8764bc4
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-08-03 10:55:36 -0300

    Global style adicionado

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './assets/styles/global.css'
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Next Level Week</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Next Level Week</h1>
+        </div>
+    );
 }
 
 export default App;
