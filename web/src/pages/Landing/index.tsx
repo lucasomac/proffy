e7d0ca2ea9ad0ab8bac8b5fd9aca74f96c08690e
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-08-03 21:17:06 -0300

    Alterado para os caminhos das rotas

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -5,6 +5,8 @@ import studyIcon from '../../assets/images/icons/study.svg'
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg'
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg'
 import './styles.css';
+import {Link} from "react-router-dom";
+
 
 function Landing() {
     return (
@@ -16,8 +18,8 @@ function Landing() {
                 </div>
                 <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
                 <div className="buttons-container">
-                    <a href="" className="study"><img src={studyIcon} alt="Estudar"/>Estudar</a>
-                    <a href="" className="give-classes"><img src={giveClassesIcon} alt="Dar aulas"/>Dar Aulas</a>
+                    <Link to="/study" className="study"><img src={studyIcon} alt="Estudar"/>Estudar</Link>
+                    <Link to="/give-classes" className="give-classes"><img src={giveClassesIcon} alt="Dar aulas"/>Dar Aulas</Link>
                 </div>
                 <span className="total-connections">
                     Total de 200 conexões já realizadas <img src={purpleHeartIcon} alt="Coração roxo"/>
