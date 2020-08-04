e003415ff9aa62d07534b41a8b5eeee69f2ca1c2
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-08-04 11:49:42 -0300

    Configuração do knexfile.ts

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -1,44 +1,49 @@
 // Update with your config settings.
+import path from "path";
 
 module.exports = {
 
-  development: {
-    client: "sqlite3",
-    connection: {
-      filename: "./dev.sqlite3"
-    }
-  },
+    development: {
+        client: "sqlite3",
+        connection: {
+            filename: path.resolve(__dirname, "src", "database", "database.sqlite")
+        },
+        migrations: {
+            directory: path.resolve(__dirname, "src", "database", "migrations")
+        },
+        useNullAsDefault: true
+    },
 
-  staging: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
+    staging: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password"
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations"
+        }
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
-    }
-  },
 
-  production: {
-    client: "postgresql",
-    connection: {
-      database: "my_db",
-      user: "username",
-      password: "password"
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: "knex_migrations"
+    production: {
+        client: "postgresql",
+        connection: {
+            database: "my_db",
+            user: "username",
+            password: "password"
+        },
+        pool: {
+            min: 2,
+            max: 10
+        },
+        migrations: {
+            tableName: "knex_migrations"
+        }
     }
-  }
 
 };
