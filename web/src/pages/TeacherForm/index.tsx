fd4ee8d8153f2a78e44e6347f7977770ca88bc53
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-08-03 21:16:05 -0300

    Adicionadas as pages teacherlist e teacherform

---
+++ web/src/pages/TeacherForm/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function TeacherForm() {
+    return (
+        <div className="">
+            <h1>Teachers</h1>
+        </div>
+    );
+}
+export default TeacherForm;
---
+++ web/src/pages/TeacherList/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+function TeacherList() {
+    return (
+        <div className="">
+            <h1>Teachers</h1>
+        </div>
+    );
+}
+export default TeacherList;
