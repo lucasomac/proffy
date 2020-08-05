6e48163c82c2300cb7cad8348ddd52ef4b00aa99
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-08-05 12:44:05 -0300

    Alteração para componente Input

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -2,24 +2,16 @@ import React from 'react';
 import PageHeader from '../../components/PageHeader';
 import TeacherItem from '../../components/TeacherItem';
 import './styles.css'
+import Input from "../../components/Input";
 
 function TeacherList() {
     return (
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <div className="input-block">
-                        <label htmlFor="subject">Matéria</label>
-                        <input type="text" id="subject"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="week-day">Dia da semana</label>
-                        <input type="text" id="week-day"/>
-                    </div>
-                    <div className="input-block">
-                        <label htmlFor="time">Hora</label>
-                        <input type="text" id="time"/>
-                    </div>
+                    <Input label="Matéria" name="subject"/>
+                    <Input label="Dia da semana" name="week-day"/>
+                    <Input type="time" label="Hora" name="time"/>
                 </form>
             </PageHeader>
             <main>
