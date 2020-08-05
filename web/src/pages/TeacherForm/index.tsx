54b67f5ceb46a736d9514d6b5712cbc3a0be5e61
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-08-05 12:41:02 -0300

    Ajuste de Css e alteração para componente Input

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,10 +1,36 @@
 import React from 'react';
 import PageHeader from "../../components/PageHeader";
+import './styles.css';
+import Input from "../../components/Input";
+import warningIcon from "../../assets/images/icons/warning.svg";
 
 function TeacherForm() {
     return (
-        <PageHeader title="Qua incrível que você quer dar aulas."
-                    description="O primeiro passo é preencher esse formulário de inscrição."/>
+        <div id="page-teacher-form" className="container">
+            <PageHeader title="Qua incrível que você quer dar aulas."
+                        description="O primeiro passo é preencher esse formulário de inscrição."/>
+            <main>
+                <fieldset>
+                    <legend>Seus Dados</legend>
+                    <Input label="Nome Completo" name="name"/>
+                    <Input label="Avatar" name="avatar"/>
+                    <Input label="Whatsapp" name="whatsapp"/>
+                </fieldset>
+                <fieldset>
+                    <legend>Sobre a aula</legend>
+                    <Input label="Matéria" name="subject"/>
+                    <Input label="Custo da sua hora por aula" name="cost"/>
+                </fieldset>
+                <footer>
+                    <p>
+                        <img src={warningIcon} alt="Aviso importante"/>
+                        Importante <br/>
+                        Preencha todos os dados!
+                    </p>
+                    <button type="button">Salvar cadastro</button>
+                </footer>
+            </main>
+        </div>
     );
 }
 
