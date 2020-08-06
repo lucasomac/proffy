8ad4df7fe8ac4e7e1d7c3c36e84a1c15fe5d9bdf
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-08-06 15:22:35 -0300

    Tabs estilizadas

--- mobile/src/pages/TeacherList/index.tsx
+++ mobile/src/pages/TeacherList/index.tsx
@@ -1,32 +1,11 @@
 import React from "react";
-import {ImageBackground, Text, View} from "react-native";
+import {View} from "react-native";
 import styles from "./styles";
-import giveClassesBgImage from '../../assets/images/give-classes-background.png'
-import {RectButton} from "react-native-gesture-handler";
-import {useNavigation} from '@react-navigation/native';
 
 export default function TeacherList() {
-    const {goBack} = useNavigation();
-
-    function handleNavigateBack() {
-        goBack();
-    }
-
     return (
         <View style={styles.container}>
-            <ImageBackground resizeMode="contain" source={giveClassesBgImage} style={styles.content}>
-                <Text style={styles.title}>
-                    Quer ser um proffy?
-                </Text>
-                <Text style={styles.description}>
-                    Para começar, você precisa se cadastrar como professor na nossa plataforma web.
-                </Text>
-            </ImageBackground>
-            <RectButton style={styles.okButton} onPress={handleNavigateBack}>
-                <Text style={styles.okButtonText}>
-                    Tudo bem!
-                </Text>
-            </RectButton>
+
         </View>
 
     );
--- mobile/src/routes/StudyTabs.tsx
+++ mobile/src/routes/StudyTabs.tsx
@@ -2,15 +2,51 @@ import React from 'react';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Favorites from "../pages/Favorites";
 import TeacherList from "../pages/TeacherList";
+import {Ionicons} from '@expo/vector-icons';
 
 const {Navigator, Screen} = createBottomTabNavigator();
 
 
 export default function StudyTabs() {
     return (
-        <Navigator>
-            <Screen name="TeacheList" component={TeacherList}/>
-            <Screen name="Favorites" component={Favorites}/>
+        <Navigator tabBarOptions={{
+            style: {
+                elevation: 0,
+                shadowOpacity: 0,
+                height: 64
+            },
+            tabStyle: {
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center'
+            },
+            iconStyle: {
+                flex: 0,
+                width: 20,
+                height: 20
+            },
+            labelStyle: {
+                fontFamily: 'Archivo_700Bold',
+                fontSize: 13,
+                marginLeft: 16,
+            },
+            inactiveBackgroundColor: '#FAFAFC',
+            activeBackgroundColor: '#EBEBF5',
+            inactiveTintColor: '#C1C1CC',
+            activeTintColor: '#32264D',
+        }}>
+            <Screen name="TeacheList" component={TeacherList} options={{
+                tabBarLabel: 'Proffys',
+                tabBarIcon: ({color, size}) => {
+                    return (<Ionicons name="ios-easel" size={size} color={color}/>);
+                }
+            }}/>
+            <Screen name=" Favorites" component={Favorites} options={{
+                tabBarLabel: 'Favoritos',
+                tabBarIcon: ({color, size}) => {
+                    return (<Ionicons name="ios-heart" size={size} color={color}/>);
+                }
+            }}/>
         </Navigator>
     );
 }
