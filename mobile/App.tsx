8d697fd1ca0633adaf06d72cd3222b15fe161d13
Lucas de Oliveira Macedo <lucasomac@outlook.com>
2020-08-06 10:04:37 -0300

    Mudança para landing no App.tsx

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,6 +1,5 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
 import Landing from "./src/pages/Landing";
 
 export default function App() {
@@ -11,12 +10,3 @@ export default function App() {
         </>
     );
 }
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-});
